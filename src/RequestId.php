fc7e23210251d5aa1f8c5f14d99cd941ab4aee9a
Leandro Silva <lansoweb@hotmail.com>
2020-12-15 08:46:14 -0300

    php 8.0, autorelease

--- src/RequestId.php
+++ src/RequestId.php
@@ -1,85 +1,110 @@
 <?php
+
+declare(strict_types=1);
+
 namespace LosMiddleware\RequestId;
 
+use InvalidArgumentException;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Server\MiddlewareInterface;
 use Psr\Http\Server\RequestHandlerInterface;
 use Ramsey\Uuid\Uuid;
+use Ramsey\Uuid\UuidInterface;
+
+use function array_merge;
+use function assert;
+use function in_array;
+use function is_int;
+use function is_string;
 
 final class RequestId implements MiddlewareInterface
 {
+    public const HEADER_NAME = 'X-Request-Id';
 
-    const HEADER_NAME = 'X-Request-Id';
+    /** @var array<array-key,mixed> */
+    private array $options;
 
-    /**
-     * @var array
-     */
-    private $options;
-
-    public function __construct($options = [])
+    /** @param array<array-key,mixed> $options */
+    public function __construct(array $options = [])
     {
         $this->options = array_merge([
-            'uuid' => false,
-            'uuid_version' => 4,
-            'uuid_ns' => null,
-            'uuid_name' => null,
+            'uuid'           => false,
+            'uuid_version'   => 4,
+            'uuid_ns'        => null,
+            'uuid_name'      => null,
             'allow_override' => false,
-            'header_name' => self::HEADER_NAME,
+            'header_name'    => self::HEADER_NAME,
         ], $options);
 
-        if (! is_numeric($this->options['uuid_version']) ||
-            ! in_array((int)$this->options['uuid_version'], [1,3,4,5])
+        if (
+            ! is_int($this->options['uuid_version']) ||
+            ! in_array($this->options['uuid_version'], [1, 3, 4, 5, 6])
         ) {
-            throw new \InvalidArgumentException('Uuid version must be 1, 3, 4 or 5');
+            throw new InvalidArgumentException('Uuid version must be 1, 3, 4, 5 or 6');
         }
-        $this->options['uuid_version'] = (int) $this->options['uuid_version'];
 
-        if (($this->options['uuid_version'] == 3 || $this->options['uuid_version'] == 5) &&
-            (empty($this->options['uuid_ns']) || empty($this->options['uuid_name']))) {
-                throw new \InvalidArgumentException('Uuid versions 3 and 5 requires uuid_version and uuid_name');
+        if (
+            ($this->options['uuid_version'] === 3 || $this->options['uuid_version'] === 5) &&
+            (empty($this->options['uuid_ns']) || empty($this->options['uuid_name']))
+        ) {
+            throw new InvalidArgumentException('Uuid versions 3 and 5 requires uuid_ns and uuid_name');
         }
     }
 
-    /**
-     * @param ServerRequestInterface $request
-     * @param RequestHandlerInterface $handler
-     * @return ResponseInterface
-     */
     public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
     {
         $uuid = '';
 
-        if ($this->options['allow_override'] || ! $request->hasHeader($this->options['header_name'])) {
-            $uuid = $this->generateId();
-            $request = $request->withHeader($this->options['header_name'], (string) $uuid);
-        } elseif ($request->hasHeader($this->options['header_name'])) {
-            $uuid = $request->getHeader($this->options['header_name'])[0];
+        $headerName = $this->options['header_name'];
+        assert(is_string($headerName));
+
+        if ($this->options['allow_override'] || ! $request->hasHeader($headerName)) {
+            $uuid    = $this->generateId();
+            $request = $request->withHeader($headerName, (string) $uuid);
+        } elseif ($request->hasHeader($headerName)) {
+            $uuid = $request->getHeader($headerName)[0];
         }
 
         $response = $handler->handle($request);
+
         if (! empty($uuid)) {
-            $response = $response->withHeader($this->options['header_name'], (string) $uuid);
+            $response = $response->withHeader($headerName, (string) $uuid);
         }
 
         return $response;
     }
 
-    /**
-     * @return Uuid
-     */
-    private function generateId()
+    private function generateId(): UuidInterface
     {
-        if ($this->options['uuid'] !== false && Uuid::isValid($this->options['uuid'])) {
-            return $this->options['uuid'];
+        if (is_string($this->options['uuid']) && Uuid::isValid($this->options['uuid'])) {
+            return Uuid::fromString($this->options['uuid']);
         }
 
-        $version = $this->options['uuid_version'];
-        $args = [
-            $this->options['uuid_ns'],
-            $this->options['uuid_name'],
-        ];
+        $version = (int) $this->options['uuid_version'];
 
-        return call_user_func_array(Uuid::class . '::uuid' . $version, $args);
+        switch ($version) {
+            case 1:
+                return Uuid::uuid1();
+
+            case 3:
+            case 5:
+                $ns   = $this->options['uuid_ns'];
+                $name = $this->options['uuid_name'];
+                assert(is_string($ns));
+                assert(is_string($name));
+
+                if ($version === 3) {
+                    return Uuid::uuid3($ns, $name);
+                }
+
+                return Uuid::uuid5($ns, $name);
+
+            case 6:
+                return Uuid::uuid6();
+
+            default:
+                return Uuid::uuid4();
+        }
     }
 }
